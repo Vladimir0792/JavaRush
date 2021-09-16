529c017e0143d740472d821bb400ac3e026bc859
Vladimir0792 <vova19927@mail.ru>
2021-09-16 11:47:24 +0300

    Дублируем текст

---
+++ toByteArray/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task19.task1915;
+
+import java.io.*;
+
+/* 
+Дублируем текст
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+        PrintStream s = System.out;
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        br.close();
+        
+        
+        
+        ByteArrayOutputStream oS = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(oS);
+        System.setOut(ps);
+        
+        testString.printSomething();
+        
+        System.setOut(s);
+        String st = oS.toString();
+        
+        FileOutputStream outStr = new FileOutputStream(a);
+        outStr.write(oS.toByteArray());
+        outStr.close();
+        System.out.println(st);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
